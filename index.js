fce0bc57e4449732e3f9b71546c8626dc839aed1
Alex B <abailon949@gmail.com>
2020-04-04 16:41:02 -0700

    Added questions to array and have inquirer working as well as axios to get the user img and email into and object

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
+const fs = require('fs');
+const axios = require('axios');
+const inquirer = require('inquirer');
 const questions = [
+    {
+        type: 'input',
+        message: 'What is the GitHub user name?',
+        name: 'username'
+        },
+        {
+        type: 'input',
+        message: 'test question two',
+        name: 'username2'
+        }  
+]
 
-];
 
-function writeToFile(fileName, data) {
-}
+inquirer
+    .prompt(questions)
+    .then( userQuestionObj => {
+        const queryUrl = `https://api.github.com/users/${ userQuestionObj.username }`;
+        axios
+        .get(queryUrl, {headers: { Authorization: 'token ecac3dcf2dbdebbff5774794b8e5cf7e2edb9f5f' }})
+        .then(res => {
+            let userData = {
+                userimg: res.data.avatar_url,
+                userEmail: res.data.email,
+            }
+            console.log(userData)
+        })
+    })
+    .catch(error => {
+        if(error) {
+          console.log(error)
+        }
+    })
 
-function init() {
 
-}
 
-init();
+// function writeToFile(fileName, data) {
+// }
+
+// function init() {
+
+// }
+
+// init();
