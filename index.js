f3e4164736945ec4d3c189b8200e79dc6772bb7b
Alex B <abailon949@gmail.com>
2020-04-04 21:06:00 -0700

    added text for generateMarkdown and added writeFile to index

--- index.js
+++ index.js
@@ -2,33 +2,71 @@ const fs = require('fs');
 const axios = require('axios');
 const inquirer = require('inquirer');
 require('dotenv').config()
-
+let generateMarkdown = require('./utils/generateMarkdown')
+let userData
+let projectData
 const questions = [
     {
         type: 'input',
-        message: 'What is the GitHub user name?',
+        message: 'What is your GitHub user name?',
         name: 'username'
-        },
-        {
+    },
+    {
         type: 'input',
-        message: 'test question two',
-        name: 'username2'
-        }  
+        message: 'What is the project title?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'What is the description of the project?',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What is the installation of the project?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'What is the usage of the project?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'What is the license of the project?',
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'What are the name contributors to the project?',
+        name: 'contributors'
+    },
+    {
+        type: 'input',
+        message: 'What are the test done for the project?',
+        name: 'test'
+    }
 ]
 const key = process.env.AUTHORIZATION
 
 inquirer
     .prompt(questions)
     .then( userQuestionObj => {
+        projectData = userQuestionObj
         const queryUrl = `https://api.github.com/users/${ userQuestionObj.username }`;
         axios
         .get(queryUrl, {headers: { Authorization: key }})
         .then(res => {
-            let userData = {
-                userimg: res.data.avatar_url,
-                userEmail: res.data.email,
+            userData = {
+                userImg: res.data.avatar_url,
+                userEmail: res.data.email
             }
-            console.log(userData)
+            fs.writeFile('readmeTest.md', generateMarkdown.generateMarkdown(projectData, userData), function(err) {
+                if (err) {
+                  return console.log(err);
+                }
+            })
+                        
         })
     })
     .catch(error => {
@@ -36,14 +74,3 @@ inquirer
           console.log(error)
         }
     })
-
-
-
-// function writeToFile(fileName, data) {
-// }
-
-// function init() {
-
-// }
-
-// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,48 @@
-function generateMarkdown(data) {
+function generateMarkdown(data, user) {
   return `
-# ${data.title}
+# ${ data.title }
 
+## Description
+
+${ data.description }
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+${ data.installation }
+
+## Usage
+
+${ data.usage }
+
+## License 
+
+${ data.license }
+
+## Contributing
+
+${ data.contributors }
+
+![Contributors Badge](https://img.shields.io/badge/Contributors-1-brightgreen)  
+
+
+## Tests
+
+${ data.test}
+
+## Questions 
+
+If you have any further questions feel free to contact me at ${ user.userEmail }  
+![User image](${ user.userImg })
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {generateMarkdown: generateMarkdown}
