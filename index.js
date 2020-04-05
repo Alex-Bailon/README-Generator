effd43acd9115bc033af42a3cebbb12f7650b01a
Alex B <abailon949@gmail.com>
2020-04-04 21:32:48 -0700

    removed questions from index and created question.js

--- index.js
+++ index.js
@@ -3,54 +3,14 @@ const axios = require('axios');
 const inquirer = require('inquirer');
 require('dotenv').config()
 let generateMarkdown = require('./utils/generateMarkdown')
+let questions = require('./utils/questions')
+
 let userData
 let projectData
-const questions = [
-    {
-        type: 'input',
-        message: 'What is your GitHub user name?',
-        name: 'username'
-    },
-    {
-        type: 'input',
-        message: 'What is the project title?',
-        name: 'title'
-    },
-    {
-        type: 'input',
-        message: 'What is the description of the project?',
-        name: 'description'
-    },
-    {
-        type: 'input',
-        message: 'What is the installation of the project?',
-        name: 'installation'
-    },
-    {
-        type: 'input',
-        message: 'What is the usage of the project?',
-        name: 'usage'
-    },
-    {
-        type: 'input',
-        message: 'What is the license of the project?',
-        name: 'license'
-    },
-    {
-        type: 'input',
-        message: 'What are the name contributors to the project?',
-        name: 'contributors'
-    },
-    {
-        type: 'input',
-        message: 'What are the test done for the project?',
-        name: 'test'
-    }
-]
 const key = process.env.AUTHORIZATION
 
 inquirer
-    .prompt(questions)
+    .prompt(questions.questions)
     .then( userQuestionObj => {
         projectData = userQuestionObj
         const queryUrl = `https://api.github.com/users/${ userQuestionObj.username }`;
---
+++ utils/questions.js
@@ -0,0 +1,43 @@
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your GitHub user name?',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'What is the project title?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'What is the description of the project?',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What is the installation of the project?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'What is the usage of the project?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'What is the license of the project?',
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'What are the name contributors to the project?',
+        name: 'contributors'
+    },
+    {
+        type: 'input',
+        message: 'What are the test done for the project?',
+        name: 'test'
+    }
+]
+module.exports = {questions: questions}
